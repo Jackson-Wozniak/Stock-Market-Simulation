b6f9e21e3b255906e6c1d49433ef42957a83578f
Jackson Wozniak <jjw30@comcast.net>
2022-10-10 20:27:40 -0400

    more specific endpoints for index fund controller

--- Backend/main/java/org/api/stockmarket/indexfund/controller/IndexFundController.java
+++ Backend/main/java/org/api/stockmarket/indexfund/controller/IndexFundController.java
@@ -2,10 +2,14 @@ package org.api.stockmarket.indexfund.controller;
 
 import lombok.AllArgsConstructor;
 import org.api.stockmarket.indexfund.enums.FundTracking;
+import org.api.stockmarket.indexfund.exception.IndexFundException;
 import org.api.stockmarket.indexfund.helper.CalculateIndexFundPrice;
 import org.api.stockmarket.indexfund.model.IndexFund;
 import org.api.stockmarket.indexfund.service.IndexFundService;
+import org.api.stockmarket.indexfund.utils.Capitalize;
+import org.api.stockmarket.stocks.stock.enums.MarketCap;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,17 +38,41 @@ public class IndexFundController {
     }
 
     @RequestMapping(value = "/cap")
-    public List<IndexFund> getMarketCapFunds() {
+    public List<IndexFund> getAllMarketCapFunds() {
         return indexFundService.findIndexFundByTracker(FundTracking.MARKET_CAP);
     }
 
+    @RequestMapping(value = "/cap/{cap}")
+    public IndexFund getMarketCapFund(@PathVariable String cap) {
+        return indexFundService.findIndexFundByTracker(FundTracking.MARKET_CAP).stream()
+                .filter(fund -> fund.getName().equalsIgnoreCase(cap + " Cap Index Fund"))
+                .findFirst()
+                .orElseThrow(IndexFundException::new);
+    }
+
     @RequestMapping(value = "/sector")
-    public List<IndexFund> getSectorIndexFunds() {
+    public List<IndexFund> getAllSectorIndexFunds() {
         return indexFundService.findIndexFundByTracker(FundTracking.SECTOR);
     }
 
+    @RequestMapping(value = "/sector/{sector}")
+    public IndexFund getSectorIndexFund(@PathVariable String sector) {
+        return indexFundService.findIndexFundByTracker(FundTracking.SECTOR).stream()
+                .filter(fund -> fund.getName().equalsIgnoreCase(sector + " Fund"))
+                .findFirst()
+                .orElseThrow(IndexFundException::new);
+    }
+
     @RequestMapping(value = "/volatility")
-    public List<IndexFund> getVolatilityFunds() {
+    public List<IndexFund> getAllVolatilityFunds() {
         return indexFundService.findIndexFundByTracker(FundTracking.VOLATILITY);
+    }
+
+    @RequestMapping(value = "/volatility/{volatility}")
+    public IndexFund getVolatileIndexFund(@PathVariable String volatility) {
+        return indexFundService.findIndexFundByTracker(FundTracking.VOLATILITY).stream()
+                .filter(fund -> fund.getName().equalsIgnoreCase(volatility + " Index Fund"))
+                .findFirst()
+                .orElseThrow(IndexFundException::new);
     }
 }
