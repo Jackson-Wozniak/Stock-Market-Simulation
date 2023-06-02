543de154debe48fc45e987057e58b1dfea343bf1
Jackson-Wozniak <jjw30@comcast.net>
2023-06-01 20:49:52 -0400

    add slight differences for default stock prices

--- Backend/src/main/java/org/api/stockmarket/stocks/stock/defaults/DefaultStockPrices.java
+++ Backend/src/main/java/org/api/stockmarket/stocks/stock/defaults/DefaultStockPrices.java
@@ -2,17 +2,21 @@ package org.api.stockmarket.stocks.stock.defaults;
 
 import org.api.stockmarket.stocks.stock.enums.MarketCap;
 
+import java.util.Random;
+
 public class DefaultStockPrices {
 
-    public static final double DEFAULT_LARGE_CAP_PRICE = 100.0;
-    public static final double DEFAULT_MID_CAP_PRICE = 20.0;
-    public static final double DEFAULT_SMALL_CAP_PRICE = 5.0;
+    private static final Random random = new Random();
+
+    private static final double DEFAULT_LARGE_CAP_PRICE = 100.0;
+    private static final double DEFAULT_MID_CAP_PRICE = 20.0;
+    private static final double DEFAULT_SMALL_CAP_PRICE = 5.0;
 
     public static double getDefaultPriceWithCap(MarketCap marketCap) {
         return switch (marketCap) {
-            case Large -> DEFAULT_LARGE_CAP_PRICE;
-            case Mid -> DEFAULT_MID_CAP_PRICE;
-            case Small -> DEFAULT_SMALL_CAP_PRICE;
+            case Large -> DEFAULT_LARGE_CAP_PRICE + random.nextInt(-10, 10);
+            case Mid -> DEFAULT_MID_CAP_PRICE + random.nextInt(-5, 5);
+            case Small -> DEFAULT_SMALL_CAP_PRICE + random.nextInt(-2, 2);
         };
     }
 }
