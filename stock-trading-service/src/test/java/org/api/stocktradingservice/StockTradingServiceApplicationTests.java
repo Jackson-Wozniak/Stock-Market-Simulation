77f68643432349ca9a2b785272a243590175e26d
Jackson-Wozniak <jjw30@comcast.net>
2024-05-18 21:52:04 -0400

    add stock-trading service on port 8010, move stock-market-service port 8000

---
+++ stock-trading-service/src/test/java/org/api/stocktradingservice/StockTradingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.stocktradingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
