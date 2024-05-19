5a86639db4d8afc6616f2e825edfe62a0e0c79f3
Jackson-Wozniak <jjw30@comcast.net>
2024-05-18 21:13:53 -0400

    rename Backend directory to stock-market-service

--- stock-market-service/src/main/java/org/api/StockMarketApplication.java
+++ stock-market-service/src/main/java/org/api/StockMarketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockTradingSimulatorApplication {
+public class StockMarketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockTradingSimulatorApplication.class, args);
+		SpringApplication.run(StockMarketApplication.class, args);
 	}
 
 }
--- stock-market-service/src/test/java/org/api/StockMarketApplicationTest.java
+++ stock-market-service/src/test/java/org/api/StockMarketApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StockTradingSimulatorApplicationTest {
+class StockMarketApplicationTest {
 
     @Test
     void contextLoads() {
