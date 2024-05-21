0575a53eef96e09b69c0e8b5a992bfac944b4ecc
Jackson-Wozniak <jjw30@comcast.net>
2024-05-21 10:30:14 -0400

    move EarningsReport methods inside object to simplify constructor

--- stock-market-service/src/main/java/org/api/stockmarket/stocks/earnings/entity/EarningsReport.java
+++ stock-market-service/src/main/java/org/api/stockmarket/stocks/earnings/entity/EarningsReport.java
@@ -5,11 +5,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.api.stockmarket.stocks.earnings.defaults.DefaultEarningsMessages;
 import org.api.stockmarket.stocks.stock.entity.Stock;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.time.ZonedDateTime;
+import java.util.Random;
 
 @Entity
 @Table(name = "earnings_reports")
@@ -40,15 +43,17 @@ public class EarningsReport implements Serializable {
     @Column(name = "date_of_release")
     private ZonedDateTime dateOfRelease;
 
-    public EarningsReport(Stock stock,
-                          double estimatedEPS,
-                          double actualEPS,
-                          String reportMessage,
-                          ZonedDateTime dateOfRelease) {
+    private static final Random random = new Random();
+    private static final DecimalFormat decimalFormat = new DecimalFormat("#.##");
+    private static final double MIN_EPS = 1.0;
+    private static final double MAX_RANDOM_EPS = 3.5;
+    private static final double MAX_ACTUAL_EPS = 4.5;
+
+    public EarningsReport(Stock stock, ZonedDateTime dateOfRelease) {
         this.stock = stock;
-        this.estimatedEPS = estimatedEPS;
-        this.actualEPS = actualEPS;
-        this.reportMessage = reportMessage;
+        this.estimatedEPS = calculateEstimatedEPS();
+        this.actualEPS = calculateActualEPS();
+        this.reportMessage = generateEarningsMessage();
         this.dateOfRelease = dateOfRelease;
     }
 
@@ -60,5 +65,28 @@ public class EarningsReport implements Serializable {
     @JsonIgnore
     public boolean isNegativeEarnings(){
         return this.actualEPS - this.getEstimatedEPS() <= 1;
+    }
+
+    public double calculateEstimatedEPS() {
+        double randomEPS = MAX_RANDOM_EPS + (MAX_RANDOM_EPS - MIN_EPS) * random.nextDouble();
+        return Double.parseDouble(decimalFormat.format(randomEPS));
+    }
+
+    public double calculateActualEPS() {
+        double randomEPS = MAX_ACTUAL_EPS + (MAX_ACTUAL_EPS - MIN_EPS) * random.nextDouble();
+        return Double.parseDouble(decimalFormat.format(randomEPS));
+    }
+
+    public String generateEarningsMessage() {
+        if (actualEPS - estimatedEPS >= 1) {
+            return DefaultEarningsMessages.getPositiveEarningsReport(
+                    stock, estimatedEPS, actualEPS, dateOfRelease);
+        }
+        if (actualEPS - estimatedEPS <= -1) {
+            return DefaultEarningsMessages.getNegativeEarningsReport(
+                    stock, estimatedEPS, actualEPS, dateOfRelease);
+        }
+        return DefaultEarningsMessages.getNeutralEarningsReport(
+                stock, estimatedEPS, actualEPS, dateOfRelease);
     }
 }
--- stock-market-service/src/main/java/org/api/stockmarket/stocks/earnings/helpers/ReleaseEarningsReport.java
+++ stock-market-service/src/main/java/org/api/stockmarket/stocks/earnings/helpers/ReleaseEarningsReport.java
@@ -1,10 +1,8 @@
 package org.api.stockmarket.stocks.earnings.helpers;
 
 import lombok.AllArgsConstructor;
-import org.api.stockmarket.stocks.earnings.defaults.DefaultEarningsMessages;
 import org.api.stockmarket.stocks.earnings.entity.EarningsReport;
 import org.api.stockmarket.stocks.earnings.service.EarningsService;
-import org.api.stockmarket.stocks.earnings.utils.FindEarningsPerShare;
 import org.api.stockmarket.stocks.stock.entity.Stock;
 import org.api.stockmarket.stocks.stock.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,25 +38,6 @@ public class ReleaseEarningsReport {
     }
 
     public EarningsReport createEarningsReport(Stock stock, ZonedDateTime marketDate) {
-        double estimatedEPS = FindEarningsPerShare.getEstimatedEarningsPerShare();
-        double actualEPS = FindEarningsPerShare.getActualEarningsPerShare(stock);
-        String earningsMessage = getEarningsMessage(stock, estimatedEPS, actualEPS, marketDate);
-        return new EarningsReport(stock, estimatedEPS, actualEPS, earningsMessage, marketDate);
-    }
-
-    public String getEarningsMessage(Stock stock,
-                                     double estimatedEPS,
-                                     double actualEPS,
-                                     ZonedDateTime marketDate) {
-        if (actualEPS - estimatedEPS >= 1) {
-            return DefaultEarningsMessages.getPositiveEarningsReport(
-                    stock, estimatedEPS, actualEPS, marketDate);
-        }
-        if (actualEPS - estimatedEPS <= -1) {
-            return DefaultEarningsMessages.getNegativeEarningsReport(
-                    stock, estimatedEPS, actualEPS, marketDate);
-        }
-        return DefaultEarningsMessages.getNeutralEarningsReport(
-                stock, estimatedEPS, actualEPS, marketDate);
+        return new EarningsReport(stock, marketDate);
     }
 }
