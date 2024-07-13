d1a60bcba9c4c322c18e6f60715ef2c4247e0096
Jackson Wozniak <jjw30@comcast.net>
2024-07-13 13:00:00 -0400

    add stock logger when changing stock prices

--- stock-market-service/src/main/java/org/api/stockmarket/market/scheduled/MarketActivityScheduler.java
+++ stock-market-service/src/main/java/org/api/stockmarket/market/scheduled/MarketActivityScheduler.java
@@ -5,9 +5,14 @@ import lombok.AllArgsConstructor;
 import org.api.stockmarket.market.properties.MarketIntervals;
 import org.api.stockmarket.market.enums.TimeStamp;
 import org.api.stockmarket.market.service.MarketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.Duration;
+import java.time.Instant;
 
 @Configuration
 @EnableScheduling
@@ -16,6 +21,7 @@ public class MarketActivityScheduler {
 
     private final MarketManager marketManager;
     private final MarketService marketService;
+    private static final Logger stockLogger = LoggerFactory.getLogger("stockLogger");
 
     @Scheduled(fixedRate = MarketIntervals.ONE_SECOND)
     @SuppressWarnings("unused")
@@ -30,7 +36,10 @@ public class MarketActivityScheduler {
             marketManager.monthlyMarketActivity();
             return;
         }
+        Instant start = Instant.now();
         marketManager.hourlyMarketActivity();
+        Instant end = Instant.now();
+        stockLogger.info("{}ms", Duration.between(start, end).toMillis());
     }
 
 }
