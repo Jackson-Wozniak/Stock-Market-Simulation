7e6900f87640d3c2132c4efb34416198f967baa2
Jackson Wozniak <jjw30@comcast.net>
2025-01-25 17:45:53 -0500

    add IndexFundService testing and ensure that fund prices are rounded to 2 decimal places

--- stock-market-service/src/main/java/org/api/stockmarket/indexfund/model/IndexFund.java
+++ stock-market-service/src/main/java/org/api/stockmarket/indexfund/model/IndexFund.java
@@ -8,12 +8,17 @@ import org.api.stockmarket.indexfund.enums.FundTracking;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public abstract class IndexFund {
 
     private String name;
     private double price;
     private FundTracking fundTracking;
+
+    public IndexFund(String name, double price, FundTracking fundTracking){
+        this.name = name;
+        this.price = (Math.round(price * 100.00) / 100.00);
+        this.fundTracking = fundTracking;
+    }
 
     public abstract IndexFundDto toDto();
 }
--- stock-market-service/src/test/java/org/api/stockmarket/indexfund/service/IndexFundServiceTest.java
+++ stock-market-service/src/test/java/org/api/stockmarket/indexfund/service/IndexFundServiceTest.java
@@ -19,17 +19,48 @@ import java.util.List;
 
 public class IndexFundServiceTest {
 
+    private final List<Stock> DIFFERENT_VOLATILITIES = List.of(
+            Stock.largeCap("-", "-", "-", Volatility.EXTRA_VOLATILE, InvestorRating.Buy),
+            Stock.largeCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.largeCap("-", "-", "-", Volatility.STABLE, InvestorRating.Buy),
+            Stock.largeCap("-", "-", "-", Volatility.NORMAL, InvestorRating.Buy)
+    );
+
+    private final List<Stock> SAME_VOLATILITIES = List.of(
+            Stock.largeCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.midCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.smallCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy)
+    );
+
+    private final List<Stock> DIFFERENT_SECTORS = List.of(
+            Stock.largeCap("-", "-", "1", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.largeCap("-", "-", "2", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.largeCap("-", "-", "3", Volatility.VOLATILE, InvestorRating.Buy)
+    );
+
+    private final List<Stock> SAME_SECTORS = List.of(
+            Stock.largeCap("-", "-", "1", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.midCap("-", "-", "1", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.smallCap("-", "-", "1", Volatility.VOLATILE, InvestorRating.Buy)
+    );
+
+    private final List<Stock> DIFFERENT_CAPS = List.of(
+            Stock.largeCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.midCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.smallCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy)
+    );
+
+    private final List<Stock> LARGE_CAPS = List.of(
+            Stock.largeCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.largeCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy),
+            Stock.largeCap("-", "-", "-", Volatility.VOLATILE, InvestorRating.Buy)
+    );
+
     @Test
-    void testMarketCapFundsWithMockedStocks() {
+    void testMarketCapFunds() {
         StockService mockStockService = Mockito.mock(StockService.class);
 
-        List<Stock> mockStocks = List.of(
-                Stock.largeCap("AMZN", "Amazon", "Technology", Volatility.VOLATILE, InvestorRating.Buy),
-                Stock.midCap("AAPL", "Apple", "Technology", Volatility.VOLATILE, InvestorRating.Buy),
-                Stock.smallCap("GOOG", "Google", "Technology", Volatility.VOLATILE, InvestorRating.Buy)
-        );
-
-        when(mockStockService.getAllStocks()).thenReturn(mockStocks);
+        when(mockStockService.getAllStocks()).thenReturn(DIFFERENT_CAPS);
         IndexFundService indexFundService = new IndexFundService(mockStockService);
         List<IndexFund> indexFunds = indexFundService.findMarketCapFunds();
 
@@ -44,22 +75,72 @@ public class IndexFundServiceTest {
     }
 
     @Test
-    void testMarketCapFundWithMockedStocks() {
+    void testMarketCapFund() {
         StockService mockStockService = Mockito.mock(StockService.class);
 
-        List<Stock> mockStocks = List.of(
-                Stock.largeCap("AMZN", "Amazon", "Technology", Volatility.VOLATILE, InvestorRating.Buy),
-                Stock.largeCap("AAPL", "Apple", "Technology", Volatility.VOLATILE, InvestorRating.Buy),
-                Stock.largeCap("GOOG", "Google", "Technology", Volatility.VOLATILE, InvestorRating.Buy)
-        );
-
-        when(mockStockService.getAllStocksByMarketCap(MarketCap.Large)).thenReturn(mockStocks);
+        when(mockStockService.getAllStocksByMarketCap(MarketCap.Large)).thenReturn(LARGE_CAPS);
         IndexFundService indexFundService = new IndexFundService(mockStockService);
         IndexFund fund = indexFundService.findMarketCapFunds(MarketCap.Large);
 
         MarketCapIndexFund cap = (MarketCapIndexFund) fund;
         assertEquals(DefaultStockPrices.getDefaultPrice(cap.getMarketCap()), fund.getPrice());
 
+        verify(mockStockService, times(1)).getAllStocksByMarketCap(MarketCap.Large);
+    }
+
+    @Test
+    void testSectorFunds() {
+        StockService mockStockService = Mockito.mock(StockService.class);
+
+        when(mockStockService.getAllStocks()).thenReturn(DIFFERENT_SECTORS);
+        IndexFundService indexFundService = new IndexFundService(mockStockService);
+        List<IndexFund> funds = indexFundService.findSectorFunds();
+
+        //all mock stocks are large cap (100 price)
+        funds.forEach(fund -> assertEquals(100.0, fund.getPrice()));
+
         verify(mockStockService, times(1)).getAllStocks();
+    }
+
+    @Test
+    void testSectorFund() {
+        StockService mockStockService = Mockito.mock(StockService.class);
+
+        when(mockStockService.getAllStocksBySector("1")).thenReturn(SAME_SECTORS);
+        IndexFundService indexFundService = new IndexFundService(mockStockService);
+        IndexFund fund = indexFundService.findSectorFunds("1");
+
+        //41.67 is average of one stock of each cap
+        assertEquals(41.67, fund.getPrice());
+
+        verify(mockStockService, times(1)).getAllStocksBySector("1");
+    }
+
+    @Test
+    void testVolatilityFunds() {
+        StockService mockStockService = Mockito.mock(StockService.class);
+
+        when(mockStockService.getAllStocks()).thenReturn(DIFFERENT_VOLATILITIES);
+        IndexFundService indexFundService = new IndexFundService(mockStockService);
+        List<IndexFund> funds = indexFundService.findVolatilityFunds();
+
+        //all mock stocks are large cap (100 price)
+        funds.forEach(fund -> assertEquals(100.0, fund.getPrice()));
+
+        verify(mockStockService, times(1)).getAllStocks();
+    }
+
+    @Test
+    void testVolatilityFund() {
+        StockService mockStockService = Mockito.mock(StockService.class);
+
+        when(mockStockService.getAllStocksByVolatility(Volatility.VOLATILE)).thenReturn(SAME_VOLATILITIES);
+        IndexFundService indexFundService = new IndexFundService(mockStockService);
+        IndexFund fund = indexFundService.findVolatilityFunds(Volatility.VOLATILE);
+
+        //41.67 is average of one stock of each cap
+        assertEquals(41.67, fund.getPrice());
+
+        verify(mockStockService, times(1)).getAllStocksByVolatility(Volatility.VOLATILE);
     }
 }
