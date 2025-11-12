2301aac1a01ac3c2cc44cdc7b1be0bfde34fa92f
Jackson Wozniak <jjw30@comcast.net>
2025-11-12 16:07:11 -0500

    more tests for CSVUtils

--- stock-market-service/src/test/java/org/api/stockmarket/core/csv/CSVUtilsTest.java
+++ stock-market-service/src/test/java/org/api/stockmarket/core/csv/CSVUtilsTest.java
@@ -2,7 +2,7 @@ package org.api.stockmarket.core.csv;
 
 import org.junit.jupiter.api.Test;
 
-import javax.naming.ConfigurationException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,5 +12,31 @@ class CSVUtilsTest {
     @Test
     void readingAbsentFileThrowsError(){
         assertDoesNotThrow(() -> CSVUtils.fileContents(TestFilePath, "CSVUtilsTest"));
+
+        assertDoesNotThrow(() -> CSVUtils.lineCount(TestFilePath, "CSVUtilsTest"));
+    }
+
+    @Test
+    void lineCountIsCorrect(){
+        assertEquals(2, CSVUtils.lineCount(TestFilePath, "CSVUtilsTest"));
+    }
+
+    @Test
+    void contentsReadToArrayCorrectly(){
+        List<String[]> contents = CSVUtils.fileContents(TestFilePath, "CSVUtilsTest");
+
+        assertEquals("thislineshouldnotbeignored", String.join("", contents.get(0)));
+        assertEquals("test1test2test3test4test5test6", String.join("", contents.get(1)));
+    }
+
+    @Test
+    void toArrayMapsBack(){
+        List<String[]> arrays = CSVUtils.toArray(List.of(
+                "this,line,should,not,be,ignored", "test1,test2,test3,test4,test5,test6"
+        ));
+        String[] lineOne = {"this", "line", "should", "not", "be", "ignored"};
+        String[] lineTwo = {"test1", "test2", "test3", "test4", "test5", "test6"};
+        assertArrayEquals(lineOne, arrays.get(0));
+        assertArrayEquals(lineTwo, arrays.get(1));
     }
 }
